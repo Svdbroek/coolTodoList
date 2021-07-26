8fd0cee49a0102bb9820c1c325ebc0d680fc9a76
Svdbroek <s_in_the_water@hotmail.com>
2021-07-26 21:34:51 +0200

    created todo toggle function

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,34 @@ function updateCounters() {
 
   const todoCount = document.getElementById("todo-count");
   const uncompletedTodos = totalTodos - totalCompleted;
-  todoCount.count.innerHTML = uncompletedTodos;
+  todoCount.innerHTML = uncompletedTodos;
+}
+
+function toggleDone(event) {
+  // get the checkbox from the event object
+  const checkbox = event.target;
+
+  if (checkbox.checked) {
+    checkbox.parentElement.parentElement.className = "todo completed";
+    // change the checkbox so that it shows up as completed
+  } else {
+    checkbox.parentElement.parentElement.className = "todo";
+    // change the checkbox so that it shows up as todo
+  }
+
+  // update the counters, now that we have updated the checkbox
+  updateCounters();
+}
+
+// add a "change" event listener to every checkbox,
+
+// and use the "toggleDone" function as the callback
+
+const checkboxes = document.querySelectorAll(".todo input");
+
+for (let i = 0; i < checkboxes.length; i++) {
+  const checkbox = checkboxes[i];
+  checkbox.addEventListener("change", toggleDone);
 }
 
 updateCounters();
